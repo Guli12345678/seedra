bde883fc3440ac51948a91064f889b91248144f8
Guli12345678 <gulsanammamadieva083@gmail.com>
2025-09-24 12:25:53 +0500

    first commit

--- src/app/layout/MainLayout.tsx
+++ src/app/layout/MainLayout.tsx
@@ -1,7 +1,7 @@
 import { memo } from "react";
 import { Outlet } from "react-router-dom";
 import { Header } from "@/widgets/header";
-import { Footer } from "@/widgets/footer";
+// import { Footer } from "@/widgets/footer";
 
 const MainLayout = () => {
   return (
